83f04bedbcd19156b856984297a43364f425501e
younggwon1 <dlrjsapdlf622@naver.com>
2022-01-12 20:06:49 +0900

    객체 배열

---
+++ src/CH21_1/Book.java
@@ -0,0 +1,35 @@
+package CH21_1;
+
+public class Book {
+
+	private String title;
+	private String author;
+	
+	// 기본 생성자 생성
+	public Book() {}
+	
+	public Book(String title, String author) {
+		this.title = title;
+		this.author = author;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	public void showBookInfo() {
+		System.out.println(title + "," +author);
+	}
+}
