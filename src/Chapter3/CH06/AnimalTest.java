39857608dd089778f0b57ec82a899c66fc42f633
younggwon1 <dlrjsapdlf622@naver.com>
2022-01-19 19:44:03 +0900

    다형성 및 상속을 왜 사용하는지 코드 정리

---
+++ src/Chapter3/CH06/AnimalTest.java
@@ -0,0 +1,83 @@
+package Chapter3.CH06;
+
+import java.util.ArrayList;
+
+class Animal{
+	
+	public void move() {
+		System.out.println("동물이 움직입니다.");
+	}
+	
+	// 상위 클래스의 공통 메서드는 모든 하위 클래스에서 사용 가능
+	public void eating() {
+		
+	}
+}
+
+class Human extends Animal{
+	
+	@Override
+	public void move() {
+		System.out.println("사람이 두 발로 걷습니다.");
+	}
+	
+	public void readBooks() {
+		System.out.println("사람이 책을 읽습니다.");
+	}
+}
+
+class Tiger extends Animal{
+	
+	@Override
+	public void move() {
+		System.out.println("호랑이가 네 발로 뜁니다.");
+	}
+	
+	public void hunting() {
+		System.out.println("호랑이가 사냥을 합니다.");
+	}
+}
+
+
+class Eagle extends Animal{
+	
+	@Override
+	public void move() {
+		System.out.println("독수리가 하늘을 날아갑니다.");
+	}
+	
+	public void flying() {
+		System.out.println("독수리가 날개를 쭉 펴고 멀리 날아갑니다");
+	}
+}
+
+
+
+public class AnimalTest {
+
+	public static void main(String[] args) {
+
+		Animal hAnimal = new Human();
+		Animal tAnimal = new Tiger();
+		Animal eAnimal = new Eagle();
+		
+		AnimalTest test = new AnimalTest();
+		test.moveAnimal(hAnimal);
+		test.moveAnimal(tAnimal);
+		test.moveAnimal(eAnimal);
+		
+		ArrayList<Animal> animalList = new ArrayList<Animal>();
+		animalList.add(hAnimal);
+		animalList.add(tAnimal);
+		animalList.add(eAnimal);
+		
+		for(Animal animal : animalList) {
+			animal.move();
+		}
+	}	
+	
+	public void moveAnimal(Animal animal) {
+		animal.move();
+		
+	}
+}
